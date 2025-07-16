dfbf3854d0f7c6dd83a0c0a30948325d42c3a11c
supercat1337 <albert.bazaleev@gmail.com>
2025-07-16 14:12:13 +0300

    remove components from library

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
 // @ts-check
 export * from "./utils.js";
 export { Component, SlotToggler } from "./component/component.js";
-export { Pagination } from "./pagination/pagination.js";
-export { Table } from "./table/table.js";
-export { PaginatedTable } from "./paginatedTable/paginatedTable.js";
-export { Modal } from "./modal/modal.js";
